d494e07a848d55f6d66d35f8ba7d0e6e1388c20b
gonoji <kawarai.a.aa@m.titech.ac.jp>
2021-11-01 15:49:02 +0900

    'Title'が'Titlepopo'になりました

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -33,7 +33,7 @@ export class TitleScene extends Phaser.Scene {
     private static num:integer =0
     create():void {
         this.cameras.main.setBackgroundColor(this.bk_color)
-        this.startText = this.add.text(parseInt(this.game.config.width.toString())/2, parseInt(this.game.config.height.toString())/2, ('Title'+TitleScene.num++), this.fontStyle)
+        this.startText = this.add.text(parseInt(this.game.config.width.toString())/2, parseInt(this.game.config.height.toString())/2, ('Titlepopo'+TitleScene.num++), this.fontStyle)
         
 
         this.startText.setOrigin(0.5)
