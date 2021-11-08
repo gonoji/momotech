8cf21efbe3b6a9a08943f9456d5f4eaec3a92e50
gonoji <kawarai.a.aa@m.titech.ac.jp>
2021-11-08 20:56:31 +0900

    Update eventMessage.ts

--- src/events/eventMessage.ts
+++ src/events/eventMessage.ts
@@ -1,20 +1,63 @@
 import { GameEvent } from "./event";
 import { KeyManager } from "../utils/keyManager";
 import { EventManager } from "./eventManager";
+import { SceneManager } from "../utils/sceneManager";
 
 export class EventMessage implements GameEvent{
     private message: Phaser.GameObjects.Text;
+    private box: Phaser.GameObjects.Rectangle;
+    private nanmojime = 0;
+    private scene = SceneManager.getCurrentScene();
+    private x = Number(this.scene.game.config.width.toString()) / 2;
+    private y = Number(this.scene.game.config.height.toString()) / 2;
+
+
+
+    /** メッセージを表示するイベント
+     * @param text メッセージの内容
+     * @param event 次のイベント
+     */
     constructor(private text: string, private next?: GameEvent){
     }
     init(){
-        const x = Number(EventManager.scene.game.config.width.toString()) / 2;
-        const y = Number(EventManager.scene.game.config.height.toString()) / 2;
-        this.message = EventManager.scene.add.text(x, y, this.text, {color: 'black', fontSize: '50px'})
+
+        
+        /*
+        this.message = scene.add.text(x, y, this.text, {color: 'black', fontSize: '50px'})
             .setOrigin(0.5)
             .setPadding(0, 10, 0, 0)
             .setDepth(100);
+        */
+        
+        // 箱を作ろう
+        this.box = this.scene.add.rectangle(this.x, this.y, 2*this.x, 2*this.y, 0x00ff00, 0.5)
+            .setOrigin(0.5)
+            .setDepth(100);
+        // メッセージの長さを知ろう
+        const messageLength = this.text.length;
+        //
+
     }
     update(){
+        let textOnScreen;
+        if(this.nanmojime < this.text.length && !KeyManager.down('Z')){
+            this.nanmojime+=0.5;
+            textOnScreen = this.text.substr(0, this.nanmojime); 
+        }
+        else{
+            textOnScreen = this.text;
+        }
+        if(KeyManager.down('X')){
+            this.message.destroy();
+        this.message = this.scene.add.text(this.x, this.y, textOnScreen, {color: 'black', fontSize: '50px'})
+            .setOrigin(0.5)
+            .setPadding(0, 10, 0, 0)
+            .setDepth(100);
+
+        } 
+        
+        
+        
         if(KeyManager.down('Z')) return this.next ?? 'ends';
         return 'continues';
     }
