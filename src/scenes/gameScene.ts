5724c6f5bae32a0f53ded50b3398e9be0d36b457
Ueno Mizuki <ueno.m.ae@m.titech.ac.jp>
2021-11-10 10:26:23 +0900

    errorをコメントアウト

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -2,7 +2,7 @@ import { EventManager } from "../events/eventManager";
 import { eventTurn } from "../events/eventTurn";
 import { GameData } from "../gameData/gameData";
 //import { Field } from "../maps/field";
-import { FileIO } from "../utils/fileIO";
+//import { FileIO } from "../utils/fileIO";
 import { KeyManager } from "../utils/keyManager";
 import { SceneManager } from "../utils/sceneManager";
 import { TitleScene } from "./titleScene";
@@ -29,7 +29,7 @@ export class GameScene extends Phaser.Scene {
     create(){
         this.cameras.main.setBackgroundColor('0xeeeeee');
         //this.field = Field.generate();
-        console.log(FileIO.getText('test'));
+        //console.log(FileIO.getText('test'));
     }
     update(){
         KeyManager.update();
