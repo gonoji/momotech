4f4e931f32ed659f6da383bbbcd3e1d0bc9b3dfd
Ueno Mizuki <ueno.m.ae@m.titech.ac.jp>
2021-11-10 10:15:56 +0900

    conflict解消

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,6 +1,8 @@
 import { EventManager } from "../events/eventManager";
 import { eventTurn } from "../events/eventTurn";
 import { GameData } from "../gameData/gameData";
+//import { Field } from "../maps/field";
+import { FileIO } from "../utils/fileIO";
 import { KeyManager } from "../utils/keyManager";
 import { SceneManager } from "../utils/sceneManager";
 import { TitleScene } from "./titleScene";
@@ -17,13 +19,15 @@ export class GameScene extends Phaser.Scene {
         this.eventManager = new EventManager(eventTurn());
     }
     preload(){
+
         this.load.baseURL = 'static/images/';
 	    this.load.image('plus', 'masu_plus_128.png');
 	    this.load.image('minus', 'masu_minus_128.png');
     }
     create(){
         this.cameras.main.setBackgroundColor('0xeeeeee');
-        this.gameData.create();
+        //this.field = Field.generate();
+        console.log(FileIO.getText('test'));
     }
     update(){
         KeyManager.update();
