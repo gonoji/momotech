6ecb1e16499f4c48108b945cfbc4fc3203c1e94f
youjo-tape <youjo.tape@gmail.com>
2021-11-11 13:22:20 +0900

    EventMove 実装、EventTurn に組み込み

--- src/events/eventMove.ts
+++ src/events/eventMove.ts
@@ -1,10 +1,23 @@
+import { GameData } from "../gameData/gameData";
+import { Direction } from "../utils/direction";
+import { KeyManager } from "../utils/keyManager";
 import { GameEvent } from "./event";
 
 export class EventMove implements GameEvent<void>{
+    /** プレイヤーがフィールド上を移動するイベント
+     * @param steps 何マス進むか
+     */
+    constructor(private steps: number){
+    }
     init(){
     }
-    update(){
-        return false;
+    update(gameData: GameData){
+        for(const dir of Direction.asArray){
+            if(KeyManager.down(dir)){
+                if(gameData.players[0].moveTo(dir)) this.steps--;
+            }
+        }
+        return this.steps <= 0;
     }
     result(){
     }
