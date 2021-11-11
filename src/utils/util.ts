882e5375d1d6800ac7c7fb0cb134641ea7031e03
Ueno Mizuki <ueno.m.ae@m.titech.ac.jp>
2021-11-11 16:58:54 +0900

    getRandomDoubleを追加

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,6 +1,10 @@
 export namespace Util{
-    export function getRandomInt(max: number){
+    export function getRandomInt(max : number){
         return Math.floor(Math.random() * max);
+    }
+    
+    export function getRandomDouble(min : number ,max : number){
+        return Math.random() * (max - min) + min;
     }
 
 }
