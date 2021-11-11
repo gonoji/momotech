9d04bec95916394535b57ae986df2cc778b43c3c
Ueno Mizuki <ueno.m.ae@m.titech.ac.jp>
2021-11-11 17:50:55 +0900

    カードの基底クラスを追加

---
+++ src/gameData/cards/card.ts
@@ -0,0 +1,11 @@
+import { GameEvent } from "../../events/event";
+import { SceneManager } from "../../utils/sceneManager";
+
+export abstract class Card{
+    
+    private sprite: Phaser.GameObjects.Sprite;
+    constructor(public name : string, public cost : number = 100){
+        this.sprite = SceneManager.scene.add.sprite(100, 100, 'card').setVisible(false);
+    }
+    abstract event(): GameEvent<unknown>;
+}
