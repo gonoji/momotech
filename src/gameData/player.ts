7260d869bb625663fbb337bfd949ac3384de34bc
youjo-tape <youjo.tape@gmail.com>
2021-11-11 12:48:56 +0900

    direction 型によるリファクタリング

--- src/gameData/player.ts
+++ src/gameData/player.ts
@@ -4,16 +4,16 @@ import { Field } from "./field";
 import { Station } from "./stations/station";
 
 export class Player{
-    public current: Station;
+    public location: Station;
     private sprite: Phaser.GameObjects.Group;
 
     constructor(readonly id: number){
     }
     create(initial: Station){
-        this.current = initial;
+        this.location = initial;
         // 画像ロード周りが整備されるまで group {ellipse + text} で代用
         const size = 72;
-        const [x, y] = Field.at(this.current.x, this.current.y);
+        const [x, y] = Field.at(this.location.x, this.location.y);
         this.sprite = SceneManager.scene.add.group()
             .add(SceneManager.scene.add.ellipse(0, 0, size, size, 0x88ff00))
             .add(SceneManager.scene.add.text(0, 0, `${this.id + 1}`, {color: 'black', fontSize: '50px'}).setOrigin(0.5))
