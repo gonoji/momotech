4afa2558433670ba9bbd14f431781985d6957084
Ueno Mizuki <ueno.m.ae@m.titech.ac.jp>
2021-11-11 16:15:37 +0900

    Jsonから自動的に読み込み

--- src/utils/fileIO.ts
+++ src/utils/fileIO.ts
@@ -1,34 +1,55 @@
-import * as fs from "fs"
-import * as readline from "readline"
 import { SceneManager } from "./sceneManager";
 
 export class FileIO{
-    static baseImageURL : string;
-    static baseTextURL : string;
-    static baseJsonURL : string;
 
     static init(){
-        FileIO.importImages();
-        FileIO.importTexts();
         FileIO.importJson();
+        if(FileIO.count==2){
+            FileIO.importImages(this.getJson('imagesJson'));
+            FileIO.importTexts(this.getJson('textsJson'));
+        }
+        FileIO.count++;
     }
 
-    static importImages(baseURL : string = 'static/images/'){
-        FileIO.baseImageURL = baseURL;
-	    this.loadImage('plus', 'masu_plus_128.png');
-	    this.loadImage('minus', 'masu_minus_128.png');
-        this.loadImage('tate', 'road_tate.png');
-        this.loadImage('yoko', 'road_yoko.png');
+    static importImages(json){
+        const imageList = json.imagesJson;
+        imageList.forEach(ele => {
+            const baseURL = ele.baseURL;
+            const contents = ele.contents;
+            contents.forEach(e => {
+                this.loadImage(e.name, baseURL+e.url);
+            });
+        });
     }
 
-    static importTexts(baseURL : string = 'static/texts/'){
-        FileIO.baseTextURL = baseURL;
-        this.loadText('test', 'test.txt');
+    static importTexts(json){
+        const textList = json.textsJson;
+        textList.forEach(ele => {
+            const baseURL = ele.baseURL;
+            const contents = ele.contents;
+            contents.forEach(e => {
+                this.loadText(e.name, baseURL+e.url);
+            });
+        });
     }
+    static count : number = 0;
 
-    static importJson(baseURL : string = 'static/json/'){
-        FileIO.baseJsonURL = baseURL;
-        this.loadJson('jsonTest', 'test.json');
+    static importJson() {
+        if(FileIO.count == 0)
+            this.loadJson('json', 'static/json/json.json');
+        else if(FileIO.count==1){
+            const json = this.getJson('json');
+            this.loadJson('imagesJson', json.imagesJson.url);
+            this.loadJson('textsJson', json.textsJson.url);
+            const jsons = json.jsons;
+            jsons.forEach(ele => {
+                const baseURL = ele.baseURL;
+                const contents = ele.contents;
+                contents.forEach(e => {
+                    this.loadJson(e.name, baseURL + e.url);
+                });
+            });
+        }
     }
 
     static getText(name : string) : string[]{
@@ -59,17 +80,16 @@ export class FileIO{
 
     private static loadImage(name : string, url : string){
         const scene = SceneManager.scene;
-	    scene.load.image(name, FileIO.baseImageURL + url);
+	    scene.load.image(name, url);
     }
 
     private static loadText(name : string, url : string){
         const scene=SceneManager.scene;
-	    scene.load.text(name, FileIO.baseTextURL + url);
+	    scene.load.text(name, url);
     }
     
     private static loadJson(name : string, url : string){
         const scene=SceneManager.scene;
-	    scene.load.json(name, FileIO.baseJsonURL + url);
+	    scene.load.json(name, url);
     }
-    
 }
