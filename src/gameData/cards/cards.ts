1030d803d58142762dc198c6f4ae3e9ee592c83c
gonoji <kawarai.a.aa@m.titech.ac.jp>
2021-11-15 11:59:44 +0900

    Update cards.ts

--- src/gameData/cards/cards.ts
+++ src/gameData/cards/cards.ts
@@ -1,8 +1,17 @@
+import { CardAdvance1, CardAdvance2, CardAdvance3, CardAdvance30, CardAdvance4, CardAdvance5, CardAdvance6, CardAdvance7 } from "./cardAdvance";
 import { CardDice2, CardDice3, CardDice4, CardDice5 } from "./cardDice";
 
 export const cards = {
     'Dice2': CardDice2,
     'Dice3': CardDice3,
     'Dice4': CardDice4,
-    'Dice5': CardDice5
+    'Dice5': CardDice5,
+    'Advance1': CardAdvance1,
+    'Advance2': CardAdvance2,
+    'Advance3': CardAdvance3,
+    'Advance4': CardAdvance4,
+    'Advance5': CardAdvance5,
+    'Advance6': CardAdvance6,
+    'Advance7': CardAdvance7,
+    'Advance30': CardAdvance30,
 } as const;
