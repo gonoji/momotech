9d36f1a6835f297368a99800f775f91b42797a48
gonoji <kawarai.a.aa@m.titech.ac.jp>
2021-11-16 01:58:53 +0900

    Merge branch 'main' of https://github.com/gonoji/momotech

--- src/events/eventDice.ts
+++ src/events/eventDice.ts
@@ -31,12 +31,32 @@ export class EventDice implements GameEvent<number>{
             }
             this.message.setText(this.dices.join(' '));
         }
-        if(KeyManager.down('Z')){
+        if(KeyManager.down('Z')  || !this.rolls){
             if(!this.rolls) return { result: this.dices.reduce((x, y) => x + y) };
             this.rolls = false;
+            // animeupdateをしたい
         }
     }
     final(){
         this.message.destroy();
+    }
+
+
+    private dicePict : Phaser.GameObjects.Arc[];
+    dicePosition(numDice: number){
+        const frameRoll = 60;
+        const frameBounce1 : number[] = [];
+        const frameBounce2 : number[] = [];
+        const height1 : number[] = [];
+        const height2 : number[] = [];
+
+        for(let i = 0; i < numDice; i ++){
+            //gIDs[i] = (IndexRoll, NumIndex-1);
+            height1[i] = 150+ Math.random();
+            height2[i] = height1[i] / 5;
+            frameBounce1[i] = frameRoll/2 + 6*Math.random();
+            frameBounce2[i] = frameRoll*3/4 + 4*Math.random();
+        }
+
     }
 }
